b1d18d6dc29b0af517013b80ca96cab0cc793da4
Mush101 <matthew.watson@live.co.uk>
2020-02-01 13:16:46 +0000

    Ladders work now

---
+++ Assets/Maps/PHTiles.tsx
@@ -0,0 +1,142 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.2" name="PHTiles" tilewidth="64" tileheight="64" tilecount="18" columns="9">
+ <image source="PHTiles.png" width="576" height="128"/>
+ <tile id="0">
+  <properties>
+   <property name="isLadder" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="1">
+  <properties>
+   <property name="isLadder" type="bool" value="false"/>
+  </properties>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="64" height="64"/>
+  </objectgroup>
+ </tile>
+ <tile id="2">
+  <properties>
+   <property name="isLadder" type="bool" value="false"/>
+  </properties>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="64" height="64"/>
+  </objectgroup>
+ </tile>
+ <tile id="3">
+  <properties>
+   <property name="isLadder" type="bool" value="false"/>
+  </properties>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="64" height="64"/>
+  </objectgroup>
+ </tile>
+ <tile id="4">
+  <properties>
+   <property name="isLadder" type="bool" value="false"/>
+  </properties>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="64" height="64"/>
+  </objectgroup>
+ </tile>
+ <tile id="5">
+  <properties>
+   <property name="isLadder" type="bool" value="false"/>
+  </properties>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="64" height="64"/>
+  </objectgroup>
+ </tile>
+ <tile id="6">
+  <properties>
+   <property name="isLadder" type="bool" value="false"/>
+  </properties>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="64" height="64"/>
+  </objectgroup>
+ </tile>
+ <tile id="7">
+  <properties>
+   <property name="isLadder" type="bool" value="false"/>
+  </properties>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="64" height="64"/>
+  </objectgroup>
+ </tile>
+ <tile id="8">
+  <properties>
+   <property name="isLadder" type="bool" value="false"/>
+  </properties>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="64" height="64"/>
+  </objectgroup>
+ </tile>
+ <tile id="9">
+  <properties>
+   <property name="isLadder" type="bool" value="false"/>
+  </properties>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="64" height="64"/>
+  </objectgroup>
+ </tile>
+ <tile id="10">
+  <properties>
+   <property name="isLadder" type="bool" value="false"/>
+  </properties>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="64" height="64"/>
+  </objectgroup>
+ </tile>
+ <tile id="11">
+  <properties>
+   <property name="isLadder" type="bool" value="false"/>
+  </properties>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="64" height="64"/>
+  </objectgroup>
+ </tile>
+ <tile id="12">
+  <properties>
+   <property name="isLadder" type="bool" value="false"/>
+  </properties>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="64" height="64"/>
+  </objectgroup>
+ </tile>
+ <tile id="13">
+  <properties>
+   <property name="isLadder" type="bool" value="false"/>
+  </properties>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="64" height="64"/>
+  </objectgroup>
+ </tile>
+ <tile id="14">
+  <properties>
+   <property name="isLadder" type="bool" value="false"/>
+  </properties>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="64" height="64"/>
+  </objectgroup>
+ </tile>
+ <tile id="15">
+  <properties>
+   <property name="isLadder" type="bool" value="false"/>
+  </properties>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="64" height="64"/>
+  </objectgroup>
+ </tile>
+ <tile id="16">
+  <properties>
+   <property name="isLadder" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="17">
+  <properties>
+   <property name="isLadder" type="bool" value="false"/>
+  </properties>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="64" height="64"/>
+  </objectgroup>
+ </tile>
+</tileset>
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,46 +7,103 @@ public class Player : MonoBehaviour
 
     enum Directions{left, right, up, down}
 
-    private bool movement;
-    public float movementSpeed = 0.2f;
-    public float gridLine = 0.5f;
-    private Vector3 goal;
+    private Vector3 movement;
+    public float movementSpeed = 2.0f;
     private Directions direction;
+    private Rigidbody2D rb;
+    private bool canGoUpLadder, canGoDownLadder, isOnLadder;
 
 
     // Start is called before the first frame update
     void Start(){
-        movement = false;
-        goal = transform.position;
         direction = Directions.right;
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update(){
-        // StopAtGrid();
-        // if(!movement){
-        //     HandleInput();
-        // }
-        // transform.position = Vector3.MoveTowards(transform.position, goal, movementSpeed);
+        FigureOutLadders();
+        HandleInput();
         Animate();
     }
 
-    void StopAtGrid(){
-        // if ((transform.position - goal).magnitude == 0){
-        //     movement = false;
-        // }
+    void FixedUpdate(){
+        Move();
+    }
+
+    void FigureOutLadders(){
+        RaycastHit2D hit = Physics2D.Raycast(transform.position + new Vector3(0,-0.4f), Vector2.up, 0.4f, LayerMask.GetMask("Ladder"));
+        canGoUpLadder = hit.collider != null;
+        hit = Physics2D.Raycast(transform.position + new Vector3(0, -0.5f), -Vector2.up, 0.5f, LayerMask.GetMask("Ladder"));
+        canGoDownLadder = hit.collider != null;
+    }
+
+    void MountLadder(){
+        Physics2D.IgnoreLayerCollision(LayerMask.NameToLayer("Player"), LayerMask.NameToLayer("Ground"), true);
+        isOnLadder = true;
+        rb.gravityScale = 0.0f;
+        transform.position = new Vector3(Mathf.Round(transform.position.x+0.5f)-0.5f, transform.position.y);
+    }
+
+    void DismountLadder(){
+        Physics2D.IgnoreLayerCollision(LayerMask.NameToLayer("Player"), LayerMask.NameToLayer("Ground"), false);
+        isOnLadder = false;
+        rb.gravityScale = 1.0f;
     }
 
     void HandleInput(){
-        // if(Input.GetAxis("Horizontal") > 0){
-        //     movement = true;
-        //     goal = new Vector3(transform.position.x + gridLine, transform.position.y);
-        //     direction = Directions.right;
-        // }else if(Input.GetAxis("Horizontal") < 0){
-        //     movement = true;
-        //     goal = new Vector3(transform.position.x - gridLine, transform.position.y);
-        //     direction = Directions.left;
-        // }
+        movement = new Vector3();
+        if(!isOnLadder){
+            if(Input.GetAxis("Horizontal") > 0){
+                movement += Vector3.right;
+                direction = Directions.right;
+            }else if(Input.GetAxis("Horizontal") < 0){
+                movement += Vector3.left;
+                direction = Directions.left;
+            }else if(Input.GetAxis("Vertical") > 0){
+                if(canGoUpLadder){
+                    MountLadder();
+                }
+            }else if(Input.GetAxis("Vertical") < 0){
+                if(canGoDownLadder){
+                    MountLadder();
+                }
+            }
+        }else{
+            if(Input.GetAxis("Vertical") > 0){
+                movement += Vector3.up;
+                direction = Directions.up;
+                if (!canGoUpLadder){
+                    DismountLadder();
+                }
+            }else if(Input.GetAxis("Vertical") < 0){
+                movement -= Vector3.up;
+                direction = Directions.down;
+                if (!canGoDownLadder){
+                    DismountLadder();
+                }
+            }
+        }
+    }
+
+    void Move(){
+        // Check for walking off edges.
+        if(direction == Directions.right){
+            RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.right - Vector2.up, 1.0f, LayerMask.GetMask("Ground"));
+            if (hit.collider == null){
+                movement = new Vector3();
+            }
+        }else if (direction == Directions.left){
+            RaycastHit2D hit = Physics2D.Raycast(transform.position, - Vector2.right - Vector2.up, 1.0f, LayerMask.GetMask("Ground"));
+            if (hit.collider == null){
+                movement = new Vector3();
+            }
+        }
+        if(!isOnLadder){
+            rb.velocity = movement * movementSpeed + new Vector3(0, rb.velocity.y);
+        }else{ 
+            rb.velocity = movement * movementSpeed;
+        }
     }
 
     void Animate(){
