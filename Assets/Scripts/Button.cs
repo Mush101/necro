5b1b23fefb691cb272ce246b962735f34a021251
Mush101 <matthew.watson@live.co.uk>
2020-02-02 12:27:51 +0000

    Added Gates and Button

---
+++ Assets/Scripts/Button.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Button : MonoBehaviour
+{
+    public Sprite up, down;
+    private bool isPressed;
+    public int number = 1;
+    public static bool[] open;
+    public bool isOpen;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        open = new bool[10];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit2D hit = Physics2D.Raycast(transform.position , Vector2.down, 0.1f, LayerMask.GetMask("Player", "Skeleton"));
+        isOpen = hit.collider != null;
+
+        if(isOpen){
+            GetComponent<SpriteRenderer>().sprite = down;
+        }else{
+            GetComponent<SpriteRenderer>().sprite = up;
+        }
+
+        open[number] = isOpen;
+    }
+}
---
+++ Assets/Scripts/Gate.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gate : MonoBehaviour
+{
+
+    public bool isOpen = false;
+    private float openAmount = 1.0f;
+    private Vector3 headPos;
+    public int number = 1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        headPos = transform.position + new Vector3(0, 0.5f);
+    }
+
+    // Update is called once per frame
+    void Update(){
+
+        isOpen = Button.open[number];
+
+        if(isOpen && openAmount > 0.1f){
+            openAmount -= 0.1f;
+        }else if(!isOpen && openAmount <= 0.9f){
+            openAmount += 0.1f;
+        }
+
+        transform.localScale = new Vector3(1,openAmount);
+        transform.position = headPos - new Vector3(0,openAmount/2);
+    }
+}
